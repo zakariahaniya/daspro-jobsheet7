a899cf95992da2c155819f5216914f86fceadffb
zakariahaniya <zakalvha@gmail.com>
2024-10-09 11:08:16 +0700

    daspro jobsheet 7

--- SiakadWhile16.java
+++ SiakadWhile16.java
@@ -19,6 +19,7 @@ public class SiakadWhile16 {
             }
             if (nilai > 80 && nilai <= 100) {
                 System.out.println("Nilai mahasiswa ke-" + (i + 1) + " adalah A");
+                System.out.println("Bagus, pertahankan nilainya");
             } else if (nilai > 73 && nilai <= 80) {
                 System.out.println("Nilai mahasiswa ke-" + (i + 1) + " adalah B+");
             } else if (nilai > 65 && nilai <= 73) {
