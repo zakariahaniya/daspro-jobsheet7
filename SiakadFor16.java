fa5f53cc73d0612f6dc07dadff6b6110e4dbca4b
zakariahaniya <zakalvha@gmail.com>
2024-10-09 10:08:15 +0700

    daspro jobsheet 7

--- SiakadFor16.java
+++ SiakadFor16.java
@@ -7,6 +7,7 @@ public class SiakadFor16 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         double nilai, tertinggi = 0, terendah = 100;
+        int statusLulus = 0, statusUlang = 0;
         for (int i = 1; i <= 10; i++) {
             System.out.print("Masukkan nilai mahasiswa ke-" + i + ": ");
             nilai = sc.nextDouble();
@@ -16,8 +17,15 @@ public class SiakadFor16 {
             if (nilai < terendah) {
                 terendah = nilai;
             }
+            if (nilai >= 60) {
+                statusLulus++;
+            } else {
+                statusUlang++;
+            }
         }
         System.out.println("Nilai tetinggi: " + tertinggi);
         System.out.println("Nilai terendah: " + terendah);
+        System.out.println("Jumlah lulus: " + statusLulus);
+        System.out.println("Jumlah tidak Lulus: " + statusUlang);
     }
 }
